212138ceb462edd6b7919f410c1f5911175c640b
Lucas Wilde <lucas.wilde@adp.com>
2017-11-18 15:33:28 -0200

    Resolvendo erro No Persistence provider for EntityManager named

--- financas-ws/src/main/java/controle/TesteDAO.java
+++ financas-ws/src/main/java/controle/TesteDAO.java
@@ -1,9 +1,16 @@
 package controle;
 
+import java.util.List;
+
+import classes.Categoria;
+
 public class TesteDAO {
 
 	public static void main(String[] args) {
-		new CategoriaDAO().listar();
+		List<Categoria> lista = new CategoriaDAO().listar();
+		for(Categoria categoria : lista) {
+			System.out.println(categoria.getDescricao());
+		}
 
 	}
 
