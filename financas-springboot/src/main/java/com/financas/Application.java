f3449a6c5ff29b85dc71a1ce4ec7a776969ac935
Lucas Wilde <lucas.wilde@adp.com>
2018-02-28 19:26:57 -0300

    Acesso ao banco com metodo main

--- financas-springboot/src/main/java/com/financas/Application.java
+++ financas-springboot/src/main/java/com/financas/Application.java
@@ -1,14 +1,28 @@
 package com.financas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.financas.service.CategoriaService;
+
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner{
 	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Autowired
+	private CategoriaService categoriaService;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		System.out.println("Testando acesso ao banco com metodo main:");
+		System.out.println("--------> Todas as categorias: " + categoriaService.listar().size());
+		
 	}
 }
