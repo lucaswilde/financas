7d6c5b304b25c64fb676986d3141c54dc4e56c62
Lucas Wilde <lucas.wilde@adp.com>
2018-03-14 19:38:17 -0300

    Adicionando fontawesome versão 4.

--- financasUI/src/app/categoria/categoria.component.ts
+++ financasUI/src/app/categoria/categoria.component.ts
@@ -7,14 +7,20 @@ import {Input, Component} from '@angular/core';
 export class CategoriaComponent{
 
     // propriedades que serao lidas pelo template
-    // usando o decorator input indicamos q a propriedade pode receber valores 
+    // usando o decorator input indicamos q a propriedade pode receber valores
     @Input() desabilitado: boolean;
     @Input() descricao: string = '';
-    cod_categoria: number;
-    _id: string;
+    codCategoria: number;
 
     constructor() {
 
+    }
+
+    get isDesabilitado(): boolean {
+        if (this.desabilitado == null || this.desabilitado !== false){
+            return false;
+        }
+        return true;
     }
 
 }
--- financasUI/src/app/categoria/categoria.service.ts
+++ financasUI/src/app/categoria/categoria.service.ts
@@ -8,33 +8,40 @@ export class CategoriaService{
 
     http: Http;
     headers: Headers;
-    url: string = 'http://localhost:8080/financas/listAllCategorias';
+    //url: string = 'http://localhost:8080/financas/listAllCategorias';
+    url: string = 'http://localhost:8080/financas-springboot/v1/categorias';
 
     constructor(http: Http){
         this.headers = new Headers();
         this.http = http;
         this.headers.append('Content-Type', 'application/json');
+        //this.headers.append('Access-Control-Allow-Origin', '*');
     }
 
     listar():Observable<CategoriaComponent[]>{
+        /* node js
         return this.http.get(this.url)
             .map(res => res.json().result);
+            */
+        return this.http.get(this.url)
+            .map(res => res.json());
     }
 
     cadastra(foto: CategoriaComponent): Observable<MensagemCadastro> {
-        if(foto._id){
+        let saveUrl = this.url + "/save";
+        if(foto.codCategoria){
             // atualiza
-            return this.http.put(this.url + "/" + foto._id, JSON.stringify(foto), {headers: this.headers})
+            return this.http.put(saveUrl, JSON.stringify(foto), {headers: this.headers})
             .map(() => new MensagemCadastro('Foto alterada com sucesso', false));
         } else{
             // insere
-            return this.http.post(this.url, JSON.stringify(foto), {headers: this.headers})
+            return this.http.post(saveUrl, JSON.stringify(foto), {headers: this.headers})
             .map(() => new MensagemCadastro('Foto incluída com sucesso', true));
         }
     }
 
     remove(foto: CategoriaComponent): Observable<Response>{
-        return this.http.delete(this.url + "/" + foto._id, {headers: this.headers});
+        return this.http.delete(this.url + "/" + foto.codCategoria, {headers: this.headers});
     }
 
     buscarPorId(id:string): Observable<CategoriaComponent>{
@@ -44,7 +51,7 @@ export class CategoriaService{
 
 export class MensagemCadastro{
 
-    // por debaixo dos panos cria das propriedades `_memsagem` e `_inclusao` como privados
+    // por debaixo dos panos cria as propriedades `_memsagem` e `_inclusao` como privados
     constructor(private _mensagem: string, private _inclusao: boolean) {
         this._mensagem = _mensagem;
         this._inclusao = _inclusao;
